bc808d1de44c2a822dcca9b2fffd2141fa974ab5
hal0taso <yuya7719guitar@gmail.com>
2017-05-10 16:02:35 +0900

    cant parse execute code...

--- src/pe.py
+++ src/pe.py
@@ -3,7 +3,7 @@ import sys
 import struct
 from winnt_def import *
 
-
+printflag=False
 
 def Banner(s):
     print('{:=^60}'.format(s.__class__.__name__))
@@ -13,16 +13,19 @@ def search_str(data):
     print('{:=^60}'.format('START STRING SEARCH'))
     lstr = []
     text = ''
+    code = False
     for b in data:
-        if 0x20 <= b <= 0x7e:
+        if 0x20 <= b <= 0x7e and (not code):
             text += chr(b)
-        elif len(text) == 1: # and ord(text) == 0x20:
-            text = ''
-        elif len(text) > 0:
+        elif len(text) > 1:
             lstr.append(text)
             text = ''
+        elif len(text) == 0 and b == 0x55: # PUSH EBP = 0x55
+            code = True
+        elif code and (b in [0xc2, 0xc3, 0x90, 0x00]):
+            code = False
         else:
-            pass
+            text = ''
     else:
         for s in lstr:
             print(s)
@@ -39,7 +42,7 @@ def main():
     # まずはIMAGE_DOS_HEADERから
     idh = IMAGE_DOS_HEADER()
     io.BytesIO(r[:sizeof(IMAGE_DOS_HEADER)]).readinto(idh)
-    infoDosHeader(idh)
+    infoDosHeader(idh, printflag)
     
     # PEヘッダの位置を取得
     pe_header = idh.e_lfanew
@@ -48,20 +51,21 @@ def main():
     inh = IMAGE_NT_HEADERS32()
     io.BytesIO(r[pe_header:pe_header + sizeof(IMAGE_NT_HEADERS32)]).readinto(inh)
 
-    infoNTHeader(inh)
+    infoNTHeader(inh, printflag)
 
     
     ifh = inh.FileHeader
     ioh = inh.OptionalHeader
 
-    infoOptionalHeader(ioh)
+    infoOptionalHeader(ioh, printflag)
     
 
     section_table = pe_header + sizeof(IMAGE_NT_HEADERS32)
     ish_array = (IMAGE_SECTION_HEADER * ifh.NumberOfSections)()
 
-    infoSectionTable(ish_array, section_table, ifh.NumberOfSections, r)
+    infoSectionTable(ish_array, section_table, ifh.NumberOfSections, r, printflag)
 
+    
     search_str(r[ish_array[0].PointerToRawData:ish_array[0].PointerToRawData + ish_array[0].SizeOfRawData])
     fd.close()
 
@@ -111,7 +115,7 @@ def infoOptionalHeader(ioh, printflag=False):
         print('    FileAlignment:               0x{:08x}'.format(ioh.FileAlignment))
         print('    SizeOfImage:                 0x{:08x}'.format(ioh.SizeOfImage))
         print('    NumberOfRvaAndSizes          0x{:08x}'.format(ioh.NumberOfRvaAndSizes))
-        infoDataDir(ioh)
+        infoDataDir(ioh, printflag)
     
 
 def infoSectionTable(ish_array, section_table, section_num, r, printflag=False):
@@ -132,7 +136,11 @@ def infoSectionTable(ish_array, section_table, section_num, r, printflag=False):
             for j in range(len(iCharacteristics)):
                 if(ish_array[i].Characteristics & iCharacteristics[j]):
                     print('        {}'.format(pcszCharacteristics[j]))
-                
+            
 
+def print_raw_data(data, ptr, size):
+    print(data[ptr:ptr + size])
+
+    
 if __name__ == '__main__':
     main()
