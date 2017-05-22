91d4ec0d5fc9d39a37335551fcf01d30bdd5743a
hal0taso <yuya7719guitar@gmail.com>
2017-05-22 18:41:41 +0900

    セクション名指定での文字列検索の実装

--- src/pe.py
+++ src/pe.py
@@ -1,33 +1,35 @@
 import sys
 from winnt_def import *
 
-
-
-
 def search_str(data):
     print('{:=^60}'.format('START STRING SEARCH'))
     lstr = []
     text = ''
     code = False
     for b in data:
-        if 0x20 <= b <= 0x7e and (not code):
+        # if len(text) == 0 and b == 0x55: # PUSH EBP = 0x55
+        #     code = True
+        # elif code and (b in [0xc2, 0xc3, 0x90, 0x00]):
+        #     code = False
+        # else:
+        #     text = ''
+        if 0x20 <= b <= 0x7e: #and (not code):
             text += chr(b)
         elif len(text) > 1:
             lstr.append(text)
             text = ''
-        elif len(text) == 0 and b == 0x55: # PUSH EBP = 0x55
-            code = True
-        elif code and (b in [0xc2, 0xc3, 0x90, 0x00]):
-            code = False
         else:
             text = ''
+
     else:
         for s in lstr:
             print(s)
     print('{:=^60}'.format('END'))
 
 
-        
+def lfnull(s):
+    return '{:\0<8}'.format(s)
+    
 
 def main():
     # read pefile
@@ -65,29 +67,18 @@ def main():
     
 
     for i in range(ish.section_num):
+        print([chr(name) for name in ish.array[i].Name])
         name = ''.join([chr(name) for name in ish.array[i].Name])
         print('Name: {}, Length: {}'.format(name, len(name)))
                        
-        if (''.join([chr(name) for name in ish.array[i].Name]) == '.text'):
+        if (''.join([chr(name) for name in ish.array[i].Name]) == lfnull('.text')):
+            search_str(r[ish.array[i].PointerToRawData:ish.array[i].PointerToRawData + ish.array[i].SizeOfRawData])
+        elif (''.join([chr(name) for name in ish.array[i].Name]) == lfnull('.rodata')):
             search_str(r[ish.array[i].PointerToRawData:ish.array[i].PointerToRawData + ish.array[i].SizeOfRawData])
 
 
     fd.close()
 
-
-    
-
-
-
-    
-
-
-
-    
-
-    
-
-            
 
 def print_raw_data(data, ptr, size):
     print(data[ptr:ptr + size])
